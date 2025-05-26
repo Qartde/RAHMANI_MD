2aea74db48e529c020b48ec1c6d810cd1f6504c1
Qartde <bonifacemd2525@gmail.com>
2025-05-26 23:17:48 +0300

    Update set.js

--- set.js
+++ set.js
@@ -10,7 +10,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
     OWNER_NAME: process.env.OWNER_NAME || "Rahmani",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || "255697608274",              
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "255693629079",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
     BOT : process.env.BOT_NAME || '𝑹𝑨𝑯𝑴𝑨𝑵𝑰_𝑴𝑫',
